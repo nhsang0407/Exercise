b4f0485c73e38a38f92a97cbcb4b6ed64d8851b0
nhsang0407 <sangnh234111e@st.uel.edu.vn>
2025-03-04 12:08:23 +0700

    Delete Exercise_KTLT/Ex118 directory

--- Exercise_KTLT/Ex118/LearnPyQtGraphPart1/TestQtGraph1.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-
-from Exercise_KTLT.Ex118.LearnPyQtGraphPart1.MainWindowQtGraph1Ext import MainWindowQtGraph1Ext
-
-app=QApplication([])
-mywindow= MainWindowQtGraph1Ext()
-mywindow.setupUi(QMainWindow())
-mywindow.show()
-app.exec()
