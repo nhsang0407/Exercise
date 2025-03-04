2eb11cb7ad2d1e40190f622f9ee3c5c42c22754a
nhsang0407 <sangnh234111e@st.uel.edu.vn>
2025-03-04 12:08:11 +0700

    Delete Exercise_KTLT/Ex117 directory

--- Exercise_KTLT/Ex117/test/test_117.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-
-from Exercise_KTLT.Ex117.ui.MainWindow117Ext import MainWindow117Ext
-
-app=QApplication([])
-mywindow= MainWindow117Ext()
-mywindow.setupUi(QMainWindow())
-mywindow.showWindow()
-app.exec()
